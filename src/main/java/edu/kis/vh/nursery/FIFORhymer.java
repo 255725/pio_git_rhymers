98c025d3245c7b9cea9a217219afb012052f56de
Jakub <255725@edu.p.lodz.pl>
2026-04-20 20:27:09 +0200

    17 Dodanie szkieletu dokumentacji w klasie FIFORhymer

--- src/main/java/edu/kis/vh/nursery/FIFORhymer.java
+++ src/main/java/edu/kis/vh/nursery/FIFORhymer.java
@@ -1,9 +1,18 @@
 package edu.kis.vh.nursery;
 
+/**
+ * Klasa dostarczająca wyliczankę opartą na podejściu kolejki First In First Out.
+ */
 public class FIFORhymer extends DefaultCountingOutRhymer {
 
     private final DefaultCountingOutRhymer temp = new DefaultCountingOutRhymer();
 
+
+    /**
+     * Zwraca i usuwa element z wyliczanki zgodnie z zasadą FIFO.
+     *
+     * @return wartość usuniętego elementu
+     */
     @Override
     protected int countOut() {
         while (!callCheck())
